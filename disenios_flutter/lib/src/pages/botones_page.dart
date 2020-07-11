605ce9eb1c4362d99379eef2b0f26dd72dfed4b4
Josué David Hernández Ramírez <justinhernandez054@gmail.com>
2020-07-10 20:10:06 -0500

    Falta navegación, solo se clonaron diseños UI

--- disenios_flutter/lib/src/pages/botones_page.dart
+++ disenios_flutter/lib/src/pages/botones_page.dart
@@ -1,4 +1,5 @@
 import 'dart:math';
+import 'dart:ui';
 
 import 'package:flutter/material.dart';
 
@@ -8,19 +9,20 @@ class BotonesPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: <Widget>[
-          _fondoApp(),
-          SingleChildScrollView(
-            child: Column(
-              children: <Widget>[
-                _titulos(),
-              ],
-            ),
-          )
-        ],
-      ),
-    );
+        body: Stack(
+          children: <Widget>[
+            _fondoApp(),
+            SingleChildScrollView(
+              child: Column(
+                children: <Widget>[
+                  _titulos(),
+                  _botonesRedondeados(),
+                ],
+              ),
+            )
+          ],
+        ),
+        bottomNavigationBar: _bottomNavigationBar(context));
   }
 
   Widget _fondoApp() {
@@ -81,5 +83,94 @@ class BotonesPage extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  Widget _botonesRedondeados() {
+    return Table(
+      children: [
+        TableRow(children: [
+          _crearBotonRedondeado(Colors.blue, Icons.border_all, 'General'),
+          _crearBotonRedondeado(Colors.purple, Icons.alarm, 'Alarma'),
+        ]),
+        TableRow(children: [
+          _crearBotonRedondeado(Colors.amber, Icons.headset, 'Sonido'),
+          _crearBotonRedondeado(Colors.green, Icons.face, 'Personas'),
+        ]),
+        TableRow(children: [
+          _crearBotonRedondeado(Colors.limeAccent, Icons.plus_one, 'Prueba'),
+          _crearBotonRedondeado(Colors.red, Icons.delete, 'Eliminar'),
+        ]),
+        TableRow(children: [
+          _crearBotonRedondeado(Colors.black, Icons.wallpaper, 'Wallpaper'),
+          _crearBotonRedondeado(Colors.cyan, Icons.call, 'Llamada'),
+        ]),
+      ],
+    );
+  }
+
+  Widget _crearBotonRedondeado(Color color, IconData icono, String texto) {
+    return Container(
+        height: 180.0,
+        margin: EdgeInsets.all(15.0),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(20.0),
+          child: BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 10.0, sigmaY: 0.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  SizedBox(
+                    height: 5.0,
+                  ),
+                  CircleAvatar(
+                    backgroundColor: color,
+                    radius: 35.0,
+                    child: Icon(
+                      icono,
+                      color: Colors.white,
+                      size: 30.0,
+                    ),
+                  ),
+                  Text(
+                    'Cosa',
+                    style: TextStyle(color: color),
+                  ),
+                  SizedBox(
+                    height: 5.0,
+                  )
+                ],
+              )),
+        ),
+        decoration: BoxDecoration(
+          color: Color.fromRGBO(62, 66, 107, 0.7),
+          borderRadius: BorderRadius.circular(20.0),
+        ));
+  }
+
+  //Tema personalizado
+  Widget _bottomNavigationBar(BuildContext context) {
+    return Theme(
+        data: Theme.of(context).copyWith(
+            canvasColor: Color.fromRGBO(55, 57, 84, 1.0),
+            primaryColor: Colors.pinkAccent,
+            textTheme: Theme.of(context).textTheme.copyWith(
+                  caption: TextStyle(color: Color.fromRGBO(116, 117, 152, 1.0)),
+                )),
+        child: BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.calendar_today, size: 30.0),
+              title: Container(),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.bubble_chart, size: 30.0),
+              title: Container(),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.supervised_user_circle, size: 30.0),
+              title: Container(),
+            ),
+          ],
+        ));
   }
 }
